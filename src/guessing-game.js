adc29b3fde9552b5fb527758f29a129f053bcfc5
Mowgle88 <mowgle1988@gmail.com>
2021-09-02 12:27:46 +0300

    completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
 
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.end - this.start)/2 + this.start);
+        return this.mid;
     }
 
     lower() {
-
+        this.end = this.mid;
     }
 
     greater() {
-
+        this.start = this.mid;
     }
 }
 
